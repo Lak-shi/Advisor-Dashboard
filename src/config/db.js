e2b01dc0daa5d63c038bedcff06b3bed62a92fc0
Lakshita Shetty <96037951+Lak-shi@users.noreply.github.com>
2025-03-11 18:20:42 -0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,6 @@ const { Pool } = pkg;
 // Create a new PostgreSQL connection pool
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 // Log when connected successfully
